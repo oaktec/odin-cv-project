ba03b3d0e7ff654648b150994a3311fe316606d5
oaktec <54672803+oaktec@users.noreply.github.com>
2023-02-28 13:54:29 +0000

    Add toggle for edit mode to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,10 @@ import React from "react";
 function Header() {
   return (
     <div className="header-container">
+      <label htmlFor="edit_toggle" className="edit-toggle">
+        <span className="edit-toggle-span">Edit Mode:</span>
+        <input type="checkbox" id="edit_toggle" />
+      </label>
       <h1>CV Project</h1>
     </div>
   );
