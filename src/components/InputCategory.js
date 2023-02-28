3a85ba55cb163dd3e736b9ba5dabce36301fef04
oaktec <54672803+oaktec@users.noreply.github.com>
2023-02-28 14:44:14 +0000

    Add fields prop to InputCategory

--- src/components/InputCategory.js
+++ src/components/InputCategory.js
@@ -4,11 +4,22 @@ import PropTypes from "prop-types";
 function InputCategory(props) {
   InputCategory.propTypes = {
     category: PropTypes.string.isRequired,
+    fields: PropTypes.arrayOf(
+      PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        label: PropTypes.string.isRequired,
+      })
+    ).isRequired,
   };
-  const { category } = props;
+  const { category, fields } = props;
   return (
     <div className="input-category">
       <h1>{category}</h1>
+      <ul>
+        {fields.map((field) => (
+          <li key={field.name}>{field.label}</li>
+        ))}
+      </ul>
     </div>
   );
 }
--- src/components/InputPane.js
+++ src/components/InputPane.js
@@ -5,9 +5,21 @@ import InputCategory from "./InputCategory";
 function InputPane() {
   return (
     <div className="input-pane">
-      <InputCategory category="Personal Details" />
-      <InputCategory category="Education" />
-      <InputCategory category="Work Experience" />
+      <InputCategory
+        category="Personal Details"
+        fields={[
+          { name: "name", label: "Name" },
+          { name: "title", label: "Title" },
+        ]}
+      />
+      <InputCategory
+        category="Education"
+        fields={[{ name: "school", label: "School" }]}
+      />
+      <InputCategory
+        category="Work Experience"
+        fields={[{ name: "company", label: "Company" }]}
+      />
     </div>
   );
 }
